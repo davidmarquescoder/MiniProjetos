f2edc3ad19b4ea1f5df109cdc48501938fe5580c
David Marques Silva de Oliveira <104264104+David-coder-dev@users.noreply.github.com>
2022-09-08 15:01:14 -0300

    Primeiro código

---
+++ CotaçãoMoedas/main.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+cotacao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL') #Fazendo a requisição das informações
+cotacao = cotacao.json() #Transformando as informações formato JSON
+cotacao_dolar = cotacao['USDBRL']['bid'] #Buscando os valores dentro da lista
+cotacao_dolar = float(cotacao_dolar) #Transformando de str para float
+
+print(f'{cotacao_dolar:.2f}')
