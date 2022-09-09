33117f611089b28fcfe7cea14a20d741b322a03f
David Marques Silva de Oliveira <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-08 22:55:00 -0300

    Comentários

--- CotaçãoMoedas/main.py
+++ CotaçãoMoedas/main.py
@@ -7,7 +7,7 @@ cotacao = cotacao.json() #Transformando as informações formato JSON
 cotacao_dolar = cotacao['USDBRL']['bid'] #Buscando os valores dentro da lista
 cotacao_dolar = float(cotacao_dolar) #Transformando de str para float
 
-cotacao_euro = cotacao['EURBRL']['bid']
-cotacao_euro = float(cotacao_euro)
+cotacao_euro = cotacao['EURBRL']['bid'] #Buscando os valores dentro da lista
+cotacao_euro = float(cotacao_euro) #Transformando de str para float
 
-print(f'DOLAR: R$ {cotacao_dolar:.2f}\nEURO: R$ {cotacao_euro:.2f}') #Imprimindo a cotação do  dolar
+print(f'DOLAR: R$ {cotacao_dolar:.2f}\nEURO: R$ {cotacao_euro:.2f}') #Imprimindo a cotação das moedas
