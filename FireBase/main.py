52f309aa3edbaddd4e4c20a79038e96cc32807cd
David Marques Silva de Oliveira <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-09 16:22:12 -0300

    Update main.py

--- FireBase/main.py
+++ FireBase/main.py
@@ -1,6 +1,8 @@
 #Utilizando o Firebase como banco de dados para esse projeto
 import requests
 
+nome = 'david'
+
 def cadastrar():
     login = '{"Nome": "Lucas"}'
     pessoas = requests.post('https://basedados1-67a0c-default-rtdb.firebaseio.com/.json', data=login)
@@ -9,6 +11,13 @@ def cadastrar():
 
 def atualizar():
     login = '{"Nome": "David"}'
-    pessoas = requests.patch('https://basedados1-67a0c-default-rtdb.firebaseio.com/-NBYcVl8BK_ZcoJE6WKP.json', data=login)
+    pessoas = requests.patch('https://basedados1-67a0c-default-rtdb.firebaseio.com/-NBYdtRwx5tmEmCO2PNV.json', data=login)
     pessoas = pessoas.json()
     print(pessoas)
+
+def teste():
+    req = requests.get('https://basedados1-67a0c-default-rtdb.firebaseio.com/.json')
+    req = req.json()
+    print(req)
+
+teste()
