b017d1b72c90917d5e6f5fa8a4570decaef749d3
David Marques Silva de Oliveira <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-08 22:52:15 -0300

    Integração da moeda "euro"

--- CotaçãoMoedas/main.py
+++ CotaçãoMoedas/main.py
@@ -3,7 +3,11 @@ import json
 
 cotacao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL') #Fazendo a requisição das informações
 cotacao = cotacao.json() #Transformando as informações formato JSON
+
 cotacao_dolar = cotacao['USDBRL']['bid'] #Buscando os valores dentro da lista
 cotacao_dolar = float(cotacao_dolar) #Transformando de str para float
 
-print(f'DOLAR: R$ {cotacao_dolar:.2f}') #Imprimindo a cotação do  dolar
+cotacao_euro = cotacao['EURBRL']['bid']
+cotacao_euro = float(cotacao_euro)
+
+print(f'DOLAR: R$ {cotacao_dolar:.2f}\nEURO: R$ {cotacao_euro:.2f}') #Imprimindo a cotação do  dolar
