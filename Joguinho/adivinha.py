454d754f276eec70bf7c7c2cf58d93328d0dff38
David Marques Silva de Oliveira <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-11 15:01:19 -0300

    Create adivinha.py

---
+++ Joguinho/adivinha.py
@@ -0,0 +1,27 @@
+secreto  = 'carro'
+digitadas = []
+
+while True:
+    letra = input('Digite uma letra: ')
+    
+    if len(letra) > 1:
+        print('AHHH! ISSO NÃO VALE, DIGITE APENAS UMA LETRA!')
+        continue
+    
+    digitadas.append(letra)
+    print(f'\n{digitadas}\n')
+    
+    if letra in secreto:
+        print(f'UHULL! A LETRA "{letra}" EXISTE NA PALAVRA!\n')
+        
+        if len(digitadas) == len(secreto):
+            res = input('Você já achou todas as letras, tente adivinhar qual a palavra secreta: ')
+            if res == secreto:
+                print('PARABENS!!! VOCÊ ACERTOU A PALAVRA SECRETA!!!!!')
+                break
+            else:
+                print(f'Sinto muito, mas a palavra secreta era "{secreto}"\n Você perdeu =(')
+                break
+    else:
+        print(f'A LETRA "{letra}" NÃO EXISTE NA PALAVRA!')
+        digitadas.pop()
