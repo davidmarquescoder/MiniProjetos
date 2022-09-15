37cc56c5903ca0c04572885fdfdf12925f43dc82
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-15 18:20:25 -0300

    Upgrade
    mecânica de funcionamento melhorada e interação maior com o usuário, além de algumas medidas de prevenção de erros implementadas.

--- Joguinho/adivinha.py
+++ Joguinho/adivinha.py
@@ -1,9 +1,33 @@
 import os
 
-chances = 3
 secreto  = 'ferrari'
 digitadas = []
 
+os.system('cls')
+print('======ADIVINHA======')
+print('Mecânica do Game:\n1 - Tente adivinhar a palavra secreta\n2 - Só vale digitar uma letra por vez até desvendar a palavra secreta e ganhar o game!\n3 - Você pode escolher entre 3 modos de game (easy, medium, hard)\n\n\n')
+
+os.system('pause')
+os.system('cls')
+
+print('======MODO DE JOGO======\nEasy - 6 chances para acertar as letras\nMedium - 3 chances para acertar as letras\nHard - 1 chance para acertar as letras\n\n')
+while True:
+    modo = (input('Escolha o modo de jogo: '))
+    os.system('cls')
+
+    if modo == 'easy':
+        chances = 6
+        break
+    elif modo == 'medium':
+        chances = 3
+        break
+    elif modo == 'hard':
+        chances = 1
+        break
+    if modo != 'easy' and modo != 'medium' and modo != 'hard':
+        print('Digite um modo de jogo válido!!\n\n'.upper())
+        continue
+print('======O JOGO COMEÇOU======\n\n')
 while True:
     if chances <= 0:
         print('Você perdeu! suas chances acabaram =(')
@@ -18,7 +42,9 @@ while True:
         print('AHHH! ISSO NÃO VALE, DIGITE APENAS LETRAS!!!')
         continue
     if letra in digitadas:
-        print('Essa letra já foi digitada!')
+        os.system('cls')
+        print('\n\nEssa letra já foi digitada!\n\n'.upper())
+        os.system('pause')
         continue
     
     digitadas.append(letra)
@@ -28,6 +54,12 @@ while True:
     else:
         print(f'A LETRA "{letra}" NÃO EXISTE NA PALAVRA!\n')
         chances -= 1
+        if chances == 0:
+            os.system('cls')
+            print(f'Chances = {chances}\n')
+            print('O jogo acabou pra você!\nSuas chances acabaram =(\n\n')
+            break
+        print(f'Você perdeu uma de suas chances, agora você tem apenas {chances} chances')
         digitadas.pop()
     
     secreto_temp = ''
