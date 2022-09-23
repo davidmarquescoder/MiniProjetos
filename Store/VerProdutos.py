e296ab70db36f7506611bcd49e1da6c3e76058d4
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-23 19:30:28 -0300

    Update VerProdutos.py

--- Store/VerProdutos.py
+++ Store/VerProdutos.py
@@ -18,7 +18,10 @@ def ver_produtos():
         lista = cursor.fetchall()
         tm = len(lista)
         for i in range(0, tm):
-            en_ver.insert(END, f'{lista[i][0]}  {lista[i][1]}  {lista[i][3]}    QNT: {lista[i][4]}\n')
+            en_ver0.insert(END, f'{lista[i][0]}\n')
+            en_ver1.insert(END, f'{lista[i][1]}\n')
+            en_ver2.insert(END, f'{lista[i][3]}\n')
+            en_ver3.insert(END, f'{lista[i][4]}\n')
 
     #CONFIGURAÇÕES DA JANELA DE CADASTRO
     janela_vu = Toplevel()
@@ -29,8 +32,29 @@ def ver_produtos():
     lbl_nome = Label(janela_vu, text='VER LISTA DE PRODUTOS CADASTRADOS', font='Congenial 14 bold', bg='orange')
     lbl_nome.place(x=60, y=15)
 
-    en_ver = Text(janela_vu, width=48, height=12, font='Congenial 10')
-    en_ver.place(x=19, y=70)
+    lbl_des0 = Label(janela_vu, text='ID', bg='orange')
+    lbl_des0.place(x=48, y=48)
+
+    lbl_des1 = Label(janela_vu, text='NOME', bg='orange')
+    lbl_des1.place(x=117, y=48)
+
+    lbl_des2 = Label(janela_vu, text='PREÇO/V', bg='orange')
+    lbl_des2.place(x=188, y=48)
+
+    lbl_des3 = Label(janela_vu, text='QUANT', bg='orange')
+    lbl_des3.place(x=272, y=48)
+
+    en_ver0 = Text(janela_vu, width=10, height=12, font='Congenial 10')
+    en_ver0.place(x=20, y=70)
+
+    en_ver1 = Text(janela_vu, width=10, height=12, font='Congenial 10')
+    en_ver1.place(x=99, y=70)
+
+    en_ver2 = Text(janela_vu, width=10, height=12, font='Congenial 10')
+    en_ver2.place(x=178, y=70)
+
+    en_ver3 = Text(janela_vu, width=10, height=12, font='Congenial 10')
+    en_ver3.place(x=257, y=70)
 
     btn0 = Button(janela_vu, text='LISTAR PRODUTOS', width=16, height=0, font='Congenial 10 bold', command = ver)
     btn0.place(x=368, y=120)
