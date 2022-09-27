c1ef5ae035160d8b1e2b8038161c93cba59690af
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-09-26 21:25:57 -0300

    Update_26_09_2022
    Apenas estilizações na interface.

--- Store/main.py
+++ Store/main.py
@@ -5,16 +5,16 @@ from VerProdutos import ver_produtos
 
 root_main = Tk()
 root_main.title('INTERFACE COMERCIAL')
-root_main.geometry('381x360')
+root_main.geometry('381x300')
 root_main['bg'] = '#F0F8FF'
 
-btn_windowC = Button(root_main, text='Cadastrar\nMercadoria', command = janela_cadastro, height=6, font='verdana 12 bold')
-btn_windowC.place(x=10, y=10)
+btn_windowC = Button(root_main, text='CADASTRAR\nMERCADORIA', command = janela_cadastro, height=6,  width=17, font='berlim 12 bold', bg='#B0C4DE', fg='black')
+btn_windowC.place(x=5, y=10)
 
-btn_windowD = Button(root_main, text='Deletar\nMercadoria', command = deletar_produto, height=6, font='verdana 12 bold')
-btn_windowD.place(x=130, y=10)
+btn_windowD = Button(root_main, text='DELETAR\nMERCADORIA', command = deletar_produto, height=6, width=17, font='berlim 12 bold', bg='#B0C4DE', fg='black')
+btn_windowD.place(x=195, y=10)
 
-btn_windowVU = Button(root_main, text='Listar\nProdutos', command = ver_produtos, height=6, width=10, font='verdana 12 bold')
-btn_windowVU.place(x=250, y=10)
+btn_windowVU = Button(root_main, text='LISTAR\nPRODUTOS', command = ver_produtos, height=6, width=34, font='berlim 12 bold', bg='#B0C4DE', fg='black')
+btn_windowVU.place(x=15, y=150)
 
 root_main.mainloop()
