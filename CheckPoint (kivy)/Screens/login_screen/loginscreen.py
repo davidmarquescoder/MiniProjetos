0ead0d4bf18f01e032ea4850aa9be74574d74928
David Marques <104264104+davidmarquescoder@users.noreply.github.com>
2022-10-11 12:00:31 -0300

    update

--- CheckPoint (kivy)/Screens/login_screen/loginscreen.py
+++ CheckPoint (kivy)/Screens/login_screen/loginscreen.py
@@ -2,8 +2,8 @@ from kivymd.uix.screen import MDScreen
 
 class LoginScreen(MDScreen):
     def get_user(self):
-        definicao_user = 'janne010'
-        definicao_pass = 'janne.1022'
+        definicao_user = 'user010'
+        definicao_pass = 'user.1022'
         validacao_user = self.ids.user.text
         validacao_pass = self.ids.password.text
         if definicao_user == validacao_user and definicao_pass == validacao_pass:
